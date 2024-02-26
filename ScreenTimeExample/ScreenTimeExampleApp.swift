e0db4dd71559ced6d272cbe23f6e5bad89d28d64
drminh2807 <drminh2807@gmail.com>
2024-02-26 13:24:21 +0700

    Initial Commit

---
+++ ScreenTimeExample/ScreenTimeExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeExampleApp.swift
+//  ScreenTimeExample
+//
+//  Created by Doan Van Minh on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
